0b64961040825a6a4e10059af6b78285d6bf43e3
Miloud Maamar <development@maamar.fr>
2020-11-19 09:37:26 +0100

    fix build path

--- run-lp-cdnl.ps1
+++ run-lp-cdnl.ps1
@@ -1,4 +1,4 @@
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
-docker build $(Get-Location)/. -t lp-cdnl:latest
+docker build $PSScriptRoot  -t lp-cdnl:latest
 docker run -d -p 80:80  lp-cdnl:latest
