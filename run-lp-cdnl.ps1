b215cb68eaf0cb4f2e4c8fa2ed2ee56ab5270b53
Miloud Maamar <development@maamar.fr>
2020-11-19 09:43:01 +0100

    tag images

--- run-lp-cdnl.ps1
+++ run-lp-cdnl.ps1
@@ -1,4 +1,4 @@
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
-docker build $PSScriptRoot  -t lp-cdnl:latest
-docker run -d -p 80:80  lp-cdnl:latest
+docker build $PSScriptRoot  -t crazyyoshi/lp-cdnl:latest
+docker run -d -p 80:80  crazyyoshi/lp-cdnl:latest
